ed1e2e5d1a396787fa7139eb87ac4afda210c9be
NguyenTienDat377 <112878795+NguyenTienDat377@users.noreply.github.com>
2023-11-13 21:46:44 +0700

    Add files via upload

---
+++ HelloController.java
@@ -0,0 +1,83 @@
+package com.example.frontendpro;
+
+import javafx.event.ActionEvent;
+import javafx.event.EventHandler;
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.control.Label;
+import javafx.scene.control.Button;
+import javafx.scene.control.TextField;
+import javafx.scene.layout.Pane;
+
+
+import java.util.Random;
+import java.util.ResourceBundle;
+import java.net.URL;
+
+public class HelloController implements Initializable {
+    private int numOfWord = 0;
+    @FXML
+    private Label instruction;
+
+    @FXML
+    private Button deleteButton, enterButton;
+
+    @FXML
+    private Button firstButton, secondButton, thirdButton, fourthButton, fifthButton;
+
+    @FXML
+    private TextField input;
+
+    @FXML
+    private Pane wordPane;
+
+    public void initialize(URL url, ResourceBundle resourceBundle) {
+        Random generates = new Random();
+        firstButton.setText(String.valueOf((char)(generates.nextInt(26) + 'a')));
+        firstButton.setOnAction(new EventHandler<ActionEvent>() {
+            @Override
+            public void handle(ActionEvent actionEvent) {
+                input.appendText(firstButton.getText());
+            }
+        });
+        secondButton.setText(String.valueOf((char)(generates.nextInt(26) + 'a')));
+        secondButton.setOnAction(new EventHandler<ActionEvent>() {
+            @Override
+            public void handle(ActionEvent actionEvent) {
+                input.appendText(secondButton.getText());
+            }
+        });
+        thirdButton.setText(String.valueOf((char)(generates.nextInt(26) + 'a')));
+        thirdButton.setOnAction(new EventHandler<ActionEvent>() {
+            @Override
+            public void handle(ActionEvent actionEvent) {
+                input.appendText(thirdButton.getText());
+            }
+        });
+        fourthButton.setText(String.valueOf((char)(generates.nextInt(26) + 'a')));
+        fourthButton.setOnAction(new EventHandler<ActionEvent>() {
+            @Override
+            public void handle(ActionEvent actionEvent) {
+                input.appendText(fourthButton.getText());
+            }
+        });
+        fifthButton.setText(String.valueOf((char)(generates.nextInt(26) + 'a')));
+        deleteButton.setText("Delete");
+        deleteButton.setOnAction(new EventHandler<ActionEvent>() {
+            @Override
+            public void handle(ActionEvent actionEvent) {
+                input.deleteNextChar();
+            }
+        });
+        enterButton.setText("Enter");
+        enterButton.setOnAction(new EventHandler<ActionEvent>() {
+            @Override
+            public void handle(ActionEvent actionEvent) {
+                return;
+            }
+        });
+
+    }
+
+
+}
